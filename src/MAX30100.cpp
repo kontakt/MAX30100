78871ac795627889c4459914aacb2e87ba979879
Connor Huffine <chuffine@gmail.com>
2016-02-15 21:49:23 -0500

    Initial commit
    Create h and cpp

---
+++ src/MAX30100.cpp
@@ -0,0 +1,121 @@
+/*
+  Library for the Maxim MAX30100 pulse oximetry system
+  Connor Huffine/Kontakt
+  February 2016
+*/
+
+#include "Arduino.h"
+#include <Wire.h>
+#include "MAX30100.h"
+
+MAX30100::MAX30100(){
+
+}
+
+void MAX30100::setLEDs(pulseWidth pw, ledCurrent red, ledCurrent ir){
+  uint8_t reg = I2CreadByte(MAX30100_ADDRESS, MAX30100_SPO2_CONFIG);
+  reg = reg & 0xFC; // Set LED_PW to 00
+  I2CwriteByte(MAX30100_ADDRESS, MAX30100_SPO2_CONFIG, reg | pw);     // Mask LED_PW
+  I2CwriteByte(MAX30100_ADDRESS, MAX30100_LED_CONFIG, (red<<4) | ir); // write LED configs
+}
+
+void MAX30100::setSPO2(sampleRate sr){
+  uint8_t reg = I2CreadByte(MAX30100_ADDRESS, MAX30100_SPO2_CONFIG);
+  reg = reg & 0xE3; // Set SPO2_SR to 000
+  I2CwriteByte(MAX30100_ADDRESS, MAX30100_SPO2_CONFIG, reg | (sr<<2)); // Mask SPO2_SR
+  reg = I2CreadByte(MAX30100_ADDRESS, MAX30100_MODE_CONFIG);
+  reg = reg & 0xf8; // Set Mode to 000
+  I2CwriteByte(MAX30100_ADDRESS, MAX30100_SPO2_CONFIG, reg | 0x03); // Mask MODE
+}
+
+int MAX30100::getNumSamp(void){
+    uint8_t wrPtr = I2CreadByte(MAX30100_ADDRESS, MAX30100_FIFO_WR_PTR);
+    uint8_t rdPtr = I2CreadByte(MAX30100_ADDRESS, MAX30100_FIFO_RD_PTR);
+    return (abs( 16 + wrPtr - rdPtr ) % 16);
+}
+
+void MAX30100::readSensor(void){
+  uint8_t temp[4] = {0};  // Temporary buffer for read values
+  I2CreadBytes(MAX30100_ADDRESS, MAX30100_FIFO_DATA, &temp[0], 4);  // Read four times from the FIFO
+  HR = (temp[0]<<8) | temp[1];    // Combine values to get the actual number
+  SPO2 = (temp[2]<<8) | temp[3];  // Combine values to get the actual number
+}
+
+void MAX30100::shutdown(void){
+  uint8_t reg = I2CreadByte(MAX30100_ADDRESS, MAX30100_MODE_CONFIG);  // Get the current register
+  I2CwriteByte(MAX30100_ADDRESS, MAX30100_MODE_CONFIG, reg | 0x80);   // mask the SHDN bit
+}
+
+void MAX30100::reset(void){
+  uint8_t reg = I2CreadByte(MAX30100_ADDRESS, MAX30100_MODE_CONFIG);  // Get the current register
+  I2CwriteByte(MAX30100_ADDRESS, MAX30100_MODE_CONFIG, reg | 0x40);   // mask the RESET bit
+}
+
+void MAX30100::startup(void){
+  uint8_t reg = I2CreadByte(MAX30100_ADDRESS, MAX30100_MODE_CONFIG);  // Get the current register
+  I2CwriteByte(MAX30100_ADDRESS, MAX30100_MODE_CONFIG, reg & 0x7F);   // mask the SHDN bit
+}
+
+int MAX30100::getRevID(void){
+  return I2CreadByte(MAX30100_ADDRESS, MAX30100_REV_ID);
+}
+
+int MAX30100::getPartID(void){
+  return I2CreadByte(MAX30100_ADDRESS, MAX30100_PART_ID);
+}
+
+void MAX30100::begin(pulseWidth pw, ledCurrent ir, sampleRate sr){
+  I2CwriteByte(MAX30100_ADDRESS, MAX30100_MODE_CONFIG, 0x02); // Heart rate only
+  I2CwriteByte(MAX30100_ADDRESS, MAX30100_LED_CONFIG, ir);
+  I2CwriteByte(MAX30100_ADDRESS, MAX30100_SPO2_CONFIG, (sr<<2)|pw);
+}
+
+void MAX30100::printRegisters(void){
+  Serial.println(I2CreadByte(MAX30100_ADDRESS, MAX30100_INT_STATUS),  BIN);
+  Serial.println(I2CreadByte(MAX30100_ADDRESS, MAX30100_INT_ENABLE),  BIN);
+  Serial.println(I2CreadByte(MAX30100_ADDRESS, MAX30100_FIFO_WR_PTR), BIN);
+  Serial.println(I2CreadByte(MAX30100_ADDRESS, MAX30100_OVRFLOW_CTR), BIN);
+  Serial.println(I2CreadByte(MAX30100_ADDRESS, MAX30100_FIFO_RD_PTR), BIN);
+  Serial.println(I2CreadByte(MAX30100_ADDRESS, MAX30100_FIFO_DATA),   BIN);
+  Serial.println(I2CreadByte(MAX30100_ADDRESS, MAX30100_MODE_CONFIG), BIN);
+  Serial.println(I2CreadByte(MAX30100_ADDRESS, MAX30100_SPO2_CONFIG), BIN);
+  Serial.println(I2CreadByte(MAX30100_ADDRESS, MAX30100_LED_CONFIG),  BIN);
+  Serial.println(I2CreadByte(MAX30100_ADDRESS, MAX30100_TEMP_INTG),   BIN);
+  Serial.println(I2CreadByte(MAX30100_ADDRESS, MAX30100_TEMP_FRAC),   BIN);
+  Serial.println(I2CreadByte(MAX30100_ADDRESS, MAX30100_REV_ID),      BIN);
+  Serial.println(I2CreadByte(MAX30100_ADDRESS, MAX30100_PART_ID),     BIN);
+}
+
+// Wire.h read and write protocols
+void MAX30100::I2CwriteByte(uint8_t address, uint8_t subAddress, uint8_t data)
+{
+	Wire.beginTransmission(address);  // Initialize the Tx buffer
+	Wire.write(subAddress);           // Put slave register address in Tx buffer
+	Wire.write(data);                 // Put data in Tx buffer
+	Wire.endTransmission();           // Send the Tx buffer
+}
+
+uint8_t MAX30100::I2CreadByte(uint8_t address, uint8_t subAddress)
+{
+	uint8_t data; // `data` will store the register data
+	Wire.beginTransmission(address);         // Initialize the Tx buffer
+	Wire.write(subAddress);	                 // Put slave register address in Tx buffer
+	Wire.endTransmission(false);             // Send the Tx buffer, but send a restart to keep connection alive
+	Wire.requestFrom(address, (uint8_t) 1);  // Read one byte from slave register address
+	data = Wire.read();                      // Fill Rx buffer with result
+	return data;                             // Return data read from slave register
+}
+
+void MAX30100::I2CreadBytes(uint8_t address, uint8_t subAddress, uint8_t * dest, uint8_t count)
+{
+	Wire.beginTransmission(address);   // Initialize the Tx buffer
+	// Next send the register to be read. OR with 0x80 to indicate multi-read.
+	Wire.write(subAddress);     // Put slave register address in Tx buffer
+	Wire.endTransmission(false);       // Send the Tx buffer, but send a restart to keep connection alive
+	uint8_t i = 0;
+	Wire.requestFrom(address, count);  // Read bytes from slave register address
+	while (Wire.available())
+	{
+		dest[i++] = Wire.read(); // Put read results in the Rx buffer
+	}
+}
---
+++ src/MAX30100.h
@@ -0,0 +1,84 @@
+/*
+  Library for the Maxim MAX30100 pulse oximetry system
+  Connor Huffine/Kontakt
+  February 2016
+*/
+
+// Registers
+#define MAX30100_INT_STATUS     0x00  // Which interrupts are tripped
+#define MAX30100_INT_ENABLE     0x01  // Which interrupts are active
+#define MAX30100_FIFO_WR_PTR    0x02  // Where data is being written
+#define MAX30100_OVRFLOW_CTR    0x03  // Number of lost samples
+#define MAX30100_FIFO_RD_PTR    0x04  // Where to read from
+#define MAX30100_FIFO_DATA      0x05  // Ouput data buffer
+#define MAX30100_MODE_CONFIG    0x06  // Control register
+#define MAX30100_SPO2_CONFIG    0x07  // Oximetry settings
+#define MAX30100_LED_CONFIG     0x09  // Pulse width and power of LEDs
+#define MAX30100_TEMP_INTG      0x16  // Temperature value, whole number
+#define MAX30100_TEMP_FRAC      0x17  // Temperature value, fraction
+#define MAX30100_REV_ID         0xFE  // Part revision
+#define MAX30100_PART_ID        0xFF  // Part ID, normally 0x11
+
+#define MAX30100_ADDRESS        0x57  // 8bit address converted to 7bit
+
+typedef enum{ // This is the same for both LEDs
+  pw200,    // 200us pulse
+  pw400,    // 400us pulse
+  pw800,    // 800us pulse
+  pw1600    // 1600us pulse
+}pulseWidth;
+
+typedef enum{
+  sr50,    // 50 samples per second
+  sr100,   // 100 samples per second
+  sr167,   // 167 samples per second
+  sr200,   // 200 samples per second
+  sr400,   // 400 samples per second
+  sr600,   // 600 samples per second
+  sr800,   // 800 samples per second
+  sr1000   // 1000 samples per second
+}sampleRate;
+
+typedef enum{
+  i0,    // No current
+  i4,    // 4.4mA
+  i8,    // 7.6mA
+  i11,   // 11.0mA
+  i14,   // 14.2mA
+  i17,   // 17.4mA
+  i21,   // 20.8mA
+  i27,   // 27.1mA
+  i31,   // 30.6mA
+  i34,   // 33.8mA
+  i37,   // 37.0mA
+  i40,   // 40.2mA
+  i44,   // 43.6mA
+  i47,   // 46.8mA
+  i50    // 50.0mA
+}ledCurrent;
+
+class MAX30100 {
+public:
+  uint16_t HR = 0;      // Last heart rate datapoint
+  uint16_t SPO2 = 0;    // Last oximetry datapoint
+
+  MAX30100();
+  void  setLEDs(pulseWidth pw, ledCurrent red, ledCurrent ir);  // Sets the LED state
+  void  setSPO2(sampleRate sr); // Setup the SPO2 sensor, disabled by default
+  int   getNumSamp(void);       // Get number of samples
+  void  readSensor(void);       // Updates the values
+  void  shutdown(void);   // Instructs device to power-save
+  void  reset(void);      // Resets the device
+  void  startup(void);    // Leaves power-save
+  int   getRevID(void);   // Gets revision ID
+  int   getPartID(void);  // Gets part ID
+  void  begin(pulseWidth pw = pw1600, // Longest pulseWidth
+              ledCurrent ir = i50,    // Highest current
+              sampleRate sr = sr100); // 2nd lowest sampleRate
+  void  printRegisters(void); // Dumps contents of registers for debug
+
+private:
+  void    I2CwriteByte(uint8_t address, uint8_t subAddress, uint8_t data);
+  uint8_t I2CreadByte(uint8_t address, uint8_t subAddress);
+  void    I2CreadBytes(uint8_t address, uint8_t subAddress, uint8_t * dest, uint8_t count);
+};
